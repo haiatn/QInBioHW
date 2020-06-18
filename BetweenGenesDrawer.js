fc0a1c573be08725d4421adf7609f4ba67330c6f
Nimrod Haiat <nimnimch@gmail.com>
2020-06-18 10:26:57 +0300

    final cmt

--- BetweenGenesDrawer.js
+++ BetweenGenesDrawer.js
@@ -1,28 +1,27 @@
-class BetweenGenesDrawer{
-    constructor(){
+class BetweenGenesDrawer {
+    constructor() {
 
     }
-    draw(results){
-        window.alert("finished");
+    draw(results) {
 
-        var speciesNames=results.species;
-        var results=results.results;
+        var speciesNames = results.species;
+        var results = results.results;
 
-        var html="<table>";
-        var titles="<tr>"
-        var values="<tr>"
-        for(var i=0; i<results.length;i++){
-            if(isNaN(results[i])){
-                titles=titles+"<td>"+speciesNames[i]+"</td>";
-            values=values+"<td>only one transcript</td>";
-            }else{
-                titles=titles+"<td>"+speciesNames[i]+"</td>";
-            values=values+"<td>"+results[i]+"</td>";
+        var html = "<table style='border: black solid 1px; padding: 5px; margin: 5px;'>";
+        var titles = "<tr>"
+        var values = "<tr>"
+        for (var i = 0; i < results.length; i++) {
+            if (isNaN(results[i])) {
+                titles = titles + "<td>" + speciesNames[i] + "</td>";
+                values = values + "<td>only one transcript</td>";
+            } else {
+                titles = titles + "<td style='border: black solid 1px;'>" + speciesNames[i] + "</td>";
+                values = values + "<td style='border: black solid 1px;'>" + results[i] + "</td>";
             }
         }
-        titles=titles+"</tr>";
-        values=values+"</tr>";
-        html=html+titles+values+"</table>";
+        titles = titles + "</tr>";
+        values = values + "</tr>";
+        html = html + titles + values + "</table>";
 
         $('#graphVisualization').html(html);
     }
--- BetweenTranscriptsComparator.js
+++ BetweenTranscriptsComparator.js
@@ -3,20 +3,32 @@ class BetweenTranscriptsComparator {
 
     }
 
-    compare(firstElement, secondElement,weights) {
+    compare(firstElement, secondElement, weights) {
         var analyzed1 = this.analyzeTranscript(firstElement);
         var analyzed2 = this.analyzeTranscript(secondElement);
 
         //attribute comparison
-        var attributeCompare=[];
+        var attributeCompare = [];
         var TXStartChange = Math.abs(analyzed1.TXStart - analyzed2.TXStart);
         var TXEndChange = Math.abs(analyzed1.TXEnd - analyzed2.TXEnd);
         var CDSStartChange = Math.abs(analyzed1.CDSStart - analyzed2.CDSStart);
         var CDSEndChange = Math.abs(analyzed1.CDSEnd - analyzed2.CDSEnd);
-        attributeCompare.push({"title":"TX Start Difference","value":TXStartChange});
-        attributeCompare.push({"title":"TX End Difference","value":TXEndChange});
-        attributeCompare.push({"title":"CDS Start Difference","value":CDSStartChange});
-        attributeCompare.push({"title":"CDS End Difference","value":CDSEndChange});
+        attributeCompare.push({
+            "title": "TX Start Difference",
+            "value": TXStartChange
+        });
+        attributeCompare.push({
+            "title": "TX End Difference",
+            "value": TXEndChange
+        });
+        attributeCompare.push({
+            "title": "CDS Start Difference",
+            "value": CDSStartChange
+        });
+        attributeCompare.push({
+            "title": "CDS End Difference",
+            "value": CDSEndChange
+        });
 
         //domain similarity
         var commonDomains = 0;
@@ -29,17 +41,27 @@ class BetweenTranscriptsComparator {
         }
         var allDomains = analyzed1.domainTypes.length + analyzed2.domainTypes.length - commonDomains;
         var domainTypeSimilarity = commonDomains / allDomains;
-        attributeCompare.push({"title":"Domain Similarity","value":(domainTypeSimilarity*100)+"%"});
+        attributeCompare.push({
+            "title": "Domain Similarity",
+            "value": (domainTypeSimilarity * 100) + "%"
+        });
 
 
         //exon similarity
-        var resultMatrix = this.getExonSimilarity(analyzed1, analyzed2,weights);
-        var totalExonSimilarity=this.getTotalExonSimilarity(resultMatrix);
-        attributeCompare.push({"title":"Exon Similarity","value":totalExonSimilarity});
+        var resultMatrix = this.getExonSimilarity(analyzed1, analyzed2, weights);
+        var totalExonSimilarity = this.getTotalExonSimilarity(resultMatrix);
+        attributeCompare.push({
+            "title": "Exon Similarity",
+            "value": totalExonSimilarity
+        });
 
         //names
-        var names=analyzed1.name+"\\"+analyzed2.name
-        return {"resultMatrix":resultMatrix,"attributeCompare":attributeCompare,'names':names};
+        var names = analyzed1.name + "\\" + analyzed2.name
+        return {
+            "resultMatrix": resultMatrix,
+            "attributeCompare": attributeCompare,
+            'names': names
+        };
 
     }
 
@@ -73,6 +95,20 @@ class BetweenTranscriptsComparator {
             exon_order_in_transcript[position].push(spliceInDomains[i].type_id);
         }
 
+        //mapping complete domains
+        for (var i = 0; i < domains.length; i++) {
+            
+            var position = this.getExonPosition(element.transcriptExons,domains[i]);
+            //if more than one exon then we have it splice in domains
+            if(position==-1){
+                continue;
+            }
+            if (exon_order_in_transcript[position] == undefined) {
+                exon_order_in_transcript[position] = [];
+            }
+            exon_order_in_transcript[position].push(domains[i].type_id);
+        }
+
         //insert to exons
         features.transcriptExons = element.transcriptExons;
         for (var i = 0; i < features.transcriptExons.length; i++) {
@@ -85,11 +121,11 @@ class BetweenTranscriptsComparator {
 
         return features; //element.exon_count;
     }
-    getExonSimilarity(firstElement, secondElement,weights) {
+    getExonSimilarity(firstElement, secondElement, weights) {
         let exons1 = firstElement.transcriptExons.sort(this.exonOrder);
         let exons2 = secondElement.transcriptExons.sort(this.exonOrder);
         let results = [];
-        let weightSum=weights[0] + weights[1] + weights[2];
+        let weightSum = weights[0] + weights[1] + weights[2];
 
         for (var i = 0; i < exons1.length; i++) {
             results[i] = [];
@@ -105,9 +141,9 @@ class BetweenTranscriptsComparator {
                 var C = this.getDomainMeasure(exons1[i].domainTypes, exons2[j].domainTypes);
 
                 if (A == 1) {
-                    results[i][j]= ( 1*weights[0] + 1*weights[1] + 1*weights[2])/weightSum;
+                    results[i][j] = (1 * weights[0] + 1 * weights[1] + 1 * weights[2]) / weightSum;
                 } else {
-                    results[i][j]= (A*weights[0] + B*weights[1] + C*weights[2])/weightSum;
+                    results[i][j] = (A * weights[0] + B * weights[1] + C * weights[2]) / weightSum;
                 }
 
             }
@@ -135,7 +171,7 @@ class BetweenTranscriptsComparator {
     getLengthMeasure(start1, end1, start2, end2) {
         var length1 = end1 - start1;
         var length2 = end2 - start2;
-        if (length1==length2 && length1!=0) {
+        if (length1 == length2 && length1 != 0) {
             return 1;
         } else {
             return 0;
@@ -158,21 +194,39 @@ class BetweenTranscriptsComparator {
         return common / (arr1.length + arr2.length - common); //union after deleting duplicates from array2
     }
 
-    getTotalExonSimilarity(resultMatrix){
-        var totalExons=resultMatrix.length+resultMatrix[0].length;
-        var sum=0;
-        for(var i=0; i<resultMatrix.length;i++){
-            sum=sum+Math.max(...resultMatrix[i]);
+    getTotalExonSimilarity(resultMatrix) {
+        var totalExons = resultMatrix.length + resultMatrix[0].length;
+        var sum = 0;
+        for (var i = 0; i < resultMatrix.length; i++) {
+            sum = sum + Math.max(...resultMatrix[i]);
         }
 
-        for(var j=0; j<resultMatrix[0].length;j++){
-            var colVal=[];
-            for(var i=0; i<resultMatrix.length;i++){
+        for (var j = 0; j < resultMatrix[0].length; j++) {
+            var colVal = [];
+            for (var i = 0; i < resultMatrix.length; i++) {
                 colVal.push(resultMatrix[i][j]);
             }
-            sum=sum+Math.max(...colVal);
+            sum = sum + Math.max(...colVal);
         }
-        return (sum*100)/(totalExons);
+        return (sum * 100) / (totalExons);
+    }
+
+    getExonPosition(exons,domain){
+        var domainStart = domain.nuc_start;
+        var exon = undefined;
+        for (var i=0; i<exons.length;i++){
+            if(exons[i].abs_start_CDS <= domainStart && domainStart <= exons[i].abs_end_CDS){
+                exon=exons[i];
+                break;
+            }
+        }
+        if(domain.nuc_end<=exon.abs_end_CDS){
+            return exon.order_in_transcript;
+        }
+        else {
+            return -1;
+        }
+        
     }
 
 
